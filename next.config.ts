6725f4683f5c97aa989b59e22015102eb31cf7dc
devnaveedabbasi <naveedabbasi03111309060@gmail.com>
2026-09-23 16:42:19 +0500

    remove proxy

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@
 const nextConfig = {
   allowedDevOrigins: ["192.168.1.46", "103.132.96.120:5000"],
 
-  async rewrites() {
-    const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: `${BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
-
 };
 
 export default nextConfig;
